fe8761b3d1245374bea8e088079a61690e0dc723
SpencerMuniz <sbottomley44@gmail.com>
2021-11-09 14:41:38 -0800

    created imports on views page

--- youtube_backend/youtubeapp/views.py
+++ youtube_backend/youtubeapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from models import Video, Comment, Reply
+from .serializers import VideoSerializer, CommentSerializer, ReplySerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import serializers, status
 
-# Create your views here.
