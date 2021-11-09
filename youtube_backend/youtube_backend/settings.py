0ce3c48869ba1ec32894b27c9bcfbdd5c87aacac
SpencerMuniz <sbottomley44@gmail.com>
2021-11-09 13:45:08 -0800

    test

--- youtube_backend/youtube_backend/settings.py
+++ youtube_backend/youtube_backend/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8@kiuho^+*&d(ihl4@*8l7h-+owz^uh-tx+4!ff+!5b8mxod1='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,12 +72,7 @@ WSGI_APPLICATION = 'youtube_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
@@ -123,3 +117,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+try:
+    from youtube_backend.local_settings import *
+except ImportError:
+    pass
