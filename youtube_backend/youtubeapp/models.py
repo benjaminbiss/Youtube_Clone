41a4f423f88f365e6e6f69918ef28d9edae8a476
SpencerMuniz <sbottomley44@gmail.com>
2021-11-11 13:31:33 -0800

    fixed some logic in the back end

--- youtube_backend/youtubeapp/models.py
+++ youtube_backend/youtubeapp/models.py
@@ -2,19 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Video(models.Model):
-    video_id = models.CharField(max_length=100)
+    video_id = models.CharField(null=True, blank=True, max_length=100)
 
 
 class Comment(models.Model):
-    video_id = models.CharField(max_length=100)
-    video_pk = models.IntegerField(null=True, blank=True)
+    video_id = models.CharField(null=True, blank=True, max_length=100)
     comment = models.CharField(max_length=250)
     likes = models.IntegerField(null=True, blank=True)
     dislikes = models.IntegerField(null=True, blank=True)
 
 
 class Reply(models.Model):
-    video_pk = models.IntegerField(null=True, blank=True)
+    video_id = models.CharField(null=True, blank=True, max_length=100)
     comment_pk = models.IntegerField(null=True, blank=True)
     reply = models.CharField(max_length=200, null=True, blank=True)
     likes = models.IntegerField(null=True, blank=True)
