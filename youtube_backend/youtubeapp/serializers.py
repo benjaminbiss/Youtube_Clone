4e718cc9617190f34a4f3794dfa4e06c33c79116
SpencerMuniz <sbottomley44@gmail.com>
2021-11-11 13:39:04 -0800

    problems with git hub

--- youtube_backend/youtubeapp/serializers.py
+++ youtube_backend/youtubeapp/serializers.py
@@ -10,15 +10,12 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ['id', 'video_id', 'comment', 'likes', 'dislikes']
-<<<<<<< HEAD
         
 class ReplySerializer(serializers.ModelSerializer):
     class Meta:
         model = Reply
         fields = ['id', 'video_id', 'comment_pk', 'reply', 'likes', 'dislikes']
-=======
 class ReplySerializer(serializers.ModelSerializer):
     class Meta:
         model = Reply
         fields = ['id', 'video_id','comment_pk', 'reply', 'likes', 'dislikes']
->>>>>>> 8f7dac78870a7cf4d0e08189032a9acc80703721
